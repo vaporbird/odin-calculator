4797c50a7449f20784e910bc6092eddec4151917
vaporbird <ivyan98@gmail.com>
2023-03-05 15:09:23 +0200

    half done project

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-
 const buttons = document.querySelectorAll('.calc-button');
 const expValue = document.querySelector('.calc-value');
 const expMemory = document.querySelector('.calc-memory');
+clearAll();
+
+function clearZeroes() {
+    const sign = expValue.textContent[0];
+    let newNum = expValue.textContent;
+    while(newNum[1] == '0'){
+        newNum = sign + newNum.substring(2, newNum.length);
+    }
+    expValue.textContent = newNum;
+}
 
 for (button of buttons) {
     button.addEventListener('click', (event) => buttonClick(event))
@@ -21,10 +30,10 @@ function calcPower(number, power) {
 }
 
 function evalExp(){
-    let split = expMemory.innerText.split(" ");
+    let split = expMemory.textContent.split(" ");
     const operator = split[1];
     const operand1 = +split[0];
-    const operand2 = +expValue.innerText;
+    const operand2 = +expValue.textContent;
     
     switch(operator){
         case '+':
@@ -42,14 +51,15 @@ function evalExp(){
 }
 
 function newResult(){
-    expValue.innerText = evalExp().toString();
-    expMemory.innerText = ' ';
+    expValue.textContent = evalExp().toString();
+    expMemory.textContent = ' ';
     return;
 }
 
 function truncateTo15() {
-    if (expValue.innerText.length > 15){
-        expValue.innerText = expValue.innerText.substring(expValue.innerText.length - 15, expValue.innerText.length)
+    const sign = expValue.textContent[0];
+    if (expValue.textContent.length > 15){
+        expValue.textContent = sign + expValue.textContent.substring(expValue.textContent.length - 14, expValue.textContent.length)
     }
     return;
 }
@@ -59,58 +69,59 @@ function buttonClick (event) {
     switch(event.target.dataset.value){
         case 'AC': clearAll();
             break;
-        case 'C': expValue.innerText != '-'? 
-                  expValue.innerText = expValue.innerText.slice(0, - 1) :
-                  true;
+        case 'C': 
+        if (expValue.textContent != '-' && expValue.textContent != ' ')
+                  expValue.textContent = expValue.textContent.slice(0, - 1) 
             break;
-        case 'sign': (expValue.innerText[0] == '-'? 
-                     expValue.innerText = expValue.innerText.slice(1) :
-                     expValue.innerText = '-' +  expValue.innerText);
+        case 'sign': (expValue.textContent[0] == '-'? 
+                     expValue.textContent = ' ' + expValue.textContent.slice(1) :
+                     expValue.textContent = '-' +  expValue.textContent);
             break;
         case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': 
-            expValue.innerText +=  event.target.dataset.value;
+            expValue.textContent +=  event.target.dataset.value;
             break;
         case '0' : case '00':
-            /[1-9]/.test(expValue.innerText)? 
-            expValue.innerText = expValue.innerText + event.target.dataset.value.toString(): 
+            /[1-9]/.test(expValue.textContent)? 
+            expValue.textContent = expValue.textContent + event.target.dataset.value.toString(): 
             true
             break;
         case '+' : case '-': case '*' : case '/': case 'mod':
-            if ((expValue.innerText != ' ' || expValue.innerText != '-') && expMemory.innerText != ' ') newResult();
-            if (/[1-9]/.test(expValue.innerText)){
-                let swap = expValue.innerText +' '+event.target.dataset.value.toString();
-                expMemory.innerText = swap;
-                expValue.innerText = ' ';
+            if ((expValue.textContent != ' ' || expValue.textContent != '-') && expMemory.textContent != ' ') newResult();
+            if (/[1-9]/.test(expValue.textContent)){
+                let swap = expValue.textContent +' '+event.target.dataset.value.toString();
+                expMemory.textContent = swap;
+                expValue.textContent = ' ';
             }
             break;
         case '.':
-            if(expValue.innerText == ' ' || expValue.innerText == '-') {
-                expValue.innerText = expValue.innerText + '0.';
+            if(expValue.textContent == ' ' || expValue.textContent == '-') {
+                expValue.textContent = expValue.textContent + '0.';
                 break;
             }
-            expValue.innerText = expValue.innerText + '.';
+            expValue.textContent = expValue.textContent + '.';
             break;
         case 'root':
-            expValue.innerText = calcPower(expValue.innerText, 0.5).toString();
+            expValue.textContent = calcPower(expValue.textContent, 0.5).toString();
             break;
         case 'sqr':
-            expValue.innerText = calcPower(expValue.innerText, 2).toString();
+            expValue.textContent = calcPower(expValue.textContent, 2).toString();
             break;
         case '=':
-            if(expMemory.innerText != ' ' || expValue.innerText == '-'){
-/*                 expValue.innerText = evalExp().toString();
-                expMemory.innerText = ''; */
+            if(expMemory.textContent != ' ' || expValue.textContent == '-'){
+/*                 expValue.textContent = evalExp().toString();
+                expMemory.textContent = ''; */
                 newResult();
             }
             break;
     }
 
     truncateTo15();
+    clearZeroes();
     return;
 }
 
 function clearAll() {
-    expValue.innerText = ' ';
-    expMemory.innerText = ' ';
+    expValue.textContent = ' ';
+    expMemory.textContent = ' ';
     return;
 }
